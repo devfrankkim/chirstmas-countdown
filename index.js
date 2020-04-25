87781fc9caaeceb402830f124e6440f756f222a2
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-04-24 20:15:35 -0400

    fixed format

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const dDayXmas = document.getElementById("time__countdown");
 
 function getTime() {
   const xmasDay = new Date("2020-12-25:00:00:00");
-  const currentDate = new Date();
-
-  const dDay = xmasDay.getTime() - currentDate.getTime();
-
-  const days = Math.floor(dDay / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((dDay / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((dDay / (1000 * 60)) % 60);
-  const seconds = Math.floor((dDay / 1000) % 60);
+  const currentTime = new Date();
+  const dDay = xmasDay - currentTime;
+  const dayMS = 1000 * 60 * 60 * 24; // 1day
+  const hourMS = 1000 * 60 * 60; // 1hour
+  const minMS = 1000 * 60; // 1min
+  const secMS = 1000; // 1sec
+  const days = Math.floor(dDay / dayMS);
+  const hours = Math.floor((dDay % dayMS) / hourMS);
+  const minutes = Math.floor(((dDay % dayMS) % hourMS) / minMS);
+  const seconds = Math.floor((((dDay % dayMS) % hourMS) % minMS) / secMS);
 
   dDayXmas.innerText = `${days < 10 ? `0${days}` : days}d ${
     hours < 10 ? `0${hours}` : hours
